1ec1f1b46f0f706bb07dcd31a0e34227a2a77674
Nicholas McGough <nicksmcgough@gmail.com>
2020-01-04 15:54:37 -0500

    Update assets.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,36 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace Controllers {
     public class PlayerController : MonoBehaviour {
-        float horizontalMove = 0f;
-        float verticalMove = 0f;
         public float moveSpeed = 5f;
         public Animator animator;
-        public Rigidbody2D rigidbody;
+        public Rigidbody2D rigidBody;
+        public GameObject weaponSlot;
+        public GameObject weapon;
+
+        Vector2 moveDirection;
 
         // Start is called before the first frame update
         void Start() {
-
+            Move();
+            weapon.transform.position = weaponSlot.transform.position;
         }
 
         // Update is called once per frame
         void Update() {
-
+            if(Input.GetButtonDown("Attack") || Input.GetButton("Attack")){
+                weapon.SetActive(true);
+            } else {
+                weapon.SetActive(false);
+            }
         }
 
         void FixedUpdate() {
-            Vector2 moveDirection =  new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            Move();
+        }
 
-//            moveSpeed = Mathf.Clamp(moveDirection.magnitude, 0.0f, 1.0f);
-            rigidbody.velocity = moveDirection * moveSpeed;
+        void Move(){
+            weapon.transform.position = weaponSlot.transform.position;
+            moveDirection =  new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            rigidBody.velocity = moveDirection * moveSpeed;
 
             if(moveDirection != Vector2.zero) {
                 animator.SetFloat("Horizontal", moveDirection.x);
                 animator.SetFloat("Vertical", moveDirection.y);
+                weapon.transform.rotation = Quaternion.LookRotation(Vector3.back, moveDirection);
+                //                Animator weaponAnimator = weapon.GetComponent<Animator>();
+                //                weaponAnimator.SetFloat("Horizontal", moveDirection.x);
+                //                weaponAnimator.SetFloat("Vertical", moveDirection.y);
             }
             animator.SetFloat("Speed", moveDirection.magnitude);
+        }
+
+        void Attack(){
+
         }
     }
 }
